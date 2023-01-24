7f265de94726d80e77cc224f6dd86679b0ca3c0b
Kevin Alejandrino <kgalejandrino@gmail.com>
2023-01-23 20:14:14 -0800

    Fixed on routing in github pages

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter as BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
-    <React.StrictMode>
+  <React.StrictMode>
+    <HashRouter>
       <App />
-    </React.StrictMode>
-  </BrowserRouter>
+    </HashRouter>
+  </React.StrictMode>
 );
